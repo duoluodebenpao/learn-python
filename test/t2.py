2ecb81355e46e15aec4071d197178086796dd2f6
duoluodebenpao <duoluodebenpao@163.com>
2019-10-29 11:49:55 +0800

    update

---
+++ test/t2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin python
+"""
+@author: fzj 
+@license: (C) Copyright 2013-2017, Node Supply Chain Manager Corporation Limited.
+@time: 2019/10/25 14:36
+@desc:
+
+"""
+
+
